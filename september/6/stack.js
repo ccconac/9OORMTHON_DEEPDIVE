128e8411504e47c6e20b8eb0efcc6feebd556fb3
ccconac <qwerpoiu91@naver.com>
2024-09-05 13:58:06 +0900

    feat: 스택 및 문자열 reverse 구현

---
+++ september/6/stack.js
@@ -0,0 +1,33 @@
+class Stack {
+  constructor() {
+    this.arr = [];
+    this.index = 0;
+  }
+
+  push(item) {
+    this.arr[this.index++] = item;
+  }
+
+  pop() {
+    if (this.index <= 0) return null;
+
+    const result = this.arr[--this.index];
+    return result;
+  }
+}
+
+const solution = (array) => {
+  const stack = new Stack();
+  const stackSize = array.length - 1;
+
+  for (let i = stackSize; i >= 0; i--) {
+    stack.push(array.split('')[i]);
+  }
+
+  return stack.arr;
+};
+
+const input = '문자열입니다';
+const answer = solution(input).join('');
+
+console.log(answer);
