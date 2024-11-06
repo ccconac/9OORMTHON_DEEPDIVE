d98261bfe52789088102ab09a8f281ce1de78dc6
ccconac <qwerpoiu91@naver.com>
2024-11-06 19:08:05 +0900

    chore: 디즈니 플러스 클론 코딩 초기 환경 설정

---
+++ october/disney-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './components/Nav';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
